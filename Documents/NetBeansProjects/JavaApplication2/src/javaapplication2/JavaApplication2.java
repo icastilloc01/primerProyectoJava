ba57c2d9c5bf16c48ad346c4f2a4de47c47dcf4b
DAM105 <DAM105@C1106.iesataulfo.local>
2020-10-08 17:55:53 +0200

    Actualización

--- Documents/NetBeansProjects/JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ Documents/NetBeansProjects/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,8 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola, soy IvÃ¡n Castillo");
+        System.out.println("Hola, soy Iván Castillo");
+        System.out.println("Actualización");
     }
     
 }
