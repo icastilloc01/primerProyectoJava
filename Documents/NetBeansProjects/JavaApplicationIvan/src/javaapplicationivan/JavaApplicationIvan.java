3829c2416f308b3165dd3fca4608ea654b949b33
DAM105 <DAM105@C1106.iesataulfo.local>
2020-10-16 16:45:45 +0200

    primer commit desde netbeans

--- Documents/NetBeansProjects/JavaApplicationIvan/src/javaapplicationivan/JavaApplicationIvan.java
+++ Documents/NetBeansProjects/JavaApplicationIvan/src/javaapplicationivan/JavaApplicationIvan.java
@@ -16,7 +16,7 @@ public class JavaApplicationIvan {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola, soy Ivan");
+        System.out.println("Hola, soy Ivan"); 
     }
     
 }
